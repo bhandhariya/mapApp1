8a407d0e8ba56e720309118ea21642af3a701e2d
raja <bhandhariya@gmail.com>
2019-10-17 13:13:11 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent  implements AfterViewInit  {
   ngAfterViewInit() {
     MapLoaderService.load().then(() => {
       this.drawPolygon();
+      this.initmapp();
     })
   }
 
@@ -41,9 +42,54 @@ export class AppComponent  implements AfterViewInit  {
       // Polygon drawn
       if (event.type === google.maps.drawing.OverlayType.POLYGON) {
         //this is the coordinate, you can assign it to a variable or pass into another function.
-        alert(event.overlay.getPath().getArray());
+        var rr=(JSON.stringify(event.overlay.getPath().getArray()));
+        console.log(JSON.parse(rr));
+        localStorage.setItem(rr,'cord')
       }
+      
     });
   }
-  
+  initmapp(){
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //   zoom: 1,
+    //   center: {lat: 24.886, lng: -70.268},
+    //   mapTypeId: 'terrain'
+    // });
+    var blueCoords = [
+      {lat: 25.774, lng: -60.190},
+      {lat: 18.466, lng: -46.118},
+      {lat: 32.321, lng: -44.757}
+    ];
+
+    var redCoords = [
+      {lat: 25.774, lng: -80.190},
+      {lat: 18.466, lng: -66.118},
+      {lat: 32.321, lng: -64.757}
+    ];
+
+    new google.maps.Polygon({
+      map: this.map,
+      paths: redCoords,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      draggable: true,
+      geodesic: true
+    });
+    new google.maps.Polygon({
+      map: this.map,
+      paths: blueCoords,
+      strokeColor: '#0000FF',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#0000FF',
+      fillOpacity: 0.35,
+      draggable: true,
+      geodesic: false
+    });
+
+
+  }
 }
